aae605b5edeff268adb38c06c9cee91f60c4b303
AnDev02 <135408714+AnDev02@users.noreply.github.com>
2024-07-25 22:57:56 +0700

    update

--- src/Routes/googleAuth.js
+++ src/Routes/googleAuth.js
@@ -2,13 +2,13 @@ const express = require("express")
 const router = express.Router()
 require("dotenv").config()
 const passport = require('passport')
-
 router.get('/google',
     passport.authenticate('google', { scope: ['profile', 'email'], session: false })
 )
 router.get('/google/callback', (req, res, next) => {
     passport.authenticate('google', (err, profile) => {
         req.user = profile
+        console.log(profile)
         next()
     })(req, res, next)
 }, (req, res) => {
